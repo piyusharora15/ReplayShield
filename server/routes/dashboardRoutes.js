f6d28d9e7fc32e977aeb9470e0f186a2aa415dad
Piyush Arora <piyusharora1504@gmail.com>
2026-05-06 11:15:51 +0530

    add: implement clear-all route to delete all transactions and attack logs

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -5,4 +5,16 @@ const dashboardController = require("../controllers/dashboardController");
 router.get("/stats", dashboardController.getStats);
 router.post("/toggle-prevention", dashboardController.togglePrevention);
 
+router.get("/clear-all", async (req, res) => {
+  try {
+    const Transaction = require("../models/Transaction");
+    const AttackLog = require("../models/AttackLog");
+    await Transaction.deleteMany({});
+    await AttackLog.deleteMany({});
+    res.json({ success: true, message: "All data cleared successfully" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+
 module.exports = router;
